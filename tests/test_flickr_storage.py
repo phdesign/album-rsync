b90b6f390d343116ab3b9519df0615ac8075ad09
Paul Heasley <paul@phdesign.com.au>
2019-01-06 21:54:32 -0500

    Testing. 1, 2, 3

--- tests/test_flickr_storage.py
+++ tests/test_flickr_storage.py
@@ -3,8 +3,9 @@ import os, sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/..')
 from unittest.mock import MagicMock, patch
 import pytest
-from album_rsync.flickr_storage import FlickrStorage
 from album_rsync.resiliently import Resiliently
+from album_rsync.flickr_storage import FlickrStorage
+from album_rsync.root_folder_info import RootFolderInfo
 
 class TestFlickrStorage:
 
@@ -24,6 +25,7 @@ class TestFlickrStorage:
         self.mock_flickr_api = self.flickr_api_patch.start()
         self.mock_flickr_api.test.login.return_value = self.user
         self.mock_flickr_api.objects.Walker = passthrough
+        self.mock_flickr_api.Photoset.create = MagicMock()
 
     def teardown_method(self):
         self.flickr_api_patch.stop()
@@ -69,8 +71,8 @@ class TestFlickrStorage:
     @pytest.fixture
     def files_fixture(self):
         return [
-            MagicMock(id='123', title='image1.jpg', tags='', originalformat='jpg'),
-            MagicMock(id='456', title='image2.jpg', tags='', originalformat='jpg')]
+            MagicMock(id='123', title='image1', tags='', originalformat='jpg'),
+            MagicMock(id='456', title='image2', tags='', originalformat='jpg')]
 
     def test_list_files_should_return_files_given_there_are_files(self, folders_fixture, files_fixture):
         self.user.getPhotosets.return_value = folders_fixture
@@ -81,68 +83,60 @@ class TestFlickrStorage:
 
         assert len(files) == 2
 
-    # def test_list_files_should_raise_not_implemented_when_root_folder_is_passed(self):
-        # storage = GoogleStorage(self.config, self.api)
-        # folder = RootFolderInfo()
-        # with pytest.raises(NotImplementedError):
-            # _ = list(storage.list_files(folder))
+    def test_list_files_should_list_files_when_root_folder_is_passed(self, files_fixture):
+        self.user.getNotInSetPhotos.return_value = files_fixture
+        storage = FlickrStorage(self.config, Resiliently(self.config))
+        folder = RootFolderInfo()
+        files = list(storage.list_files(folder))
 
-    # def test_list_files_should_not_list_file_given_its_excluded(self, files_fixture):
-        # self.config.exclude = 'image1'
-        # self.api.get_media_in_folder.return_value = files_fixture
-        # storage = GoogleStorage(self.config, self.api)
-        # folder = FolderInfo(id=123, name='test')
-        # files = list(storage.list_files(folder))
+        assert len(files) == 2
 
-        # assert len(files) == 1
-        # assert files[0].name == 'image2.jpg'
+    def test_list_files_should_not_list_file_given_its_excluded(self, folders_fixture, files_fixture):
+        self.config.exclude = 'image1'
+        self.user.getPhotosets.return_value = folders_fixture
+        folders_fixture[0].getPhotos.return_value = files_fixture
+        storage = FlickrStorage(self.config, Resiliently(self.config))
+        folders = list(storage.list_folders())
+        files = list(storage.list_files(folders[0]))
 
-    # def test_list_files_should_not_list_file_given_its_not_included(self, files_fixture):
-        # self.config.include = 'image1'
-        # self.api.get_media_in_folder.return_value = files_fixture
-        # storage = GoogleStorage(self.config, self.api)
-        # folder = FolderInfo(id=123, name='test')
-        # files = list(storage.list_files(folder))
+        assert len(files) == 1
+        assert files[0].name == 'image2.jpg'
 
-        # assert len(files) == 1
-        # assert files[0].name == 'image1.jpg'
+    def test_list_files_should_not_list_file_given_its_not_included(self, folders_fixture, files_fixture):
+        self.config.include = 'image1'
+        self.user.getPhotosets.return_value = folders_fixture
+        folders_fixture[0].getPhotos.return_value = files_fixture
+        storage = FlickrStorage(self.config, Resiliently(self.config))
+        folders = list(storage.list_folders())
+        files = list(storage.list_files(folders[0]))
 
-    # def test_list_files_should_not_list_files_given_there_are_no_files(self):
-        # self.api.get_media_in_folder.return_value = []
-        # storage = GoogleStorage(self.config, self.api)
-        # folder = FolderInfo(id=123, name='test')
-        # files = list(storage.list_files(folder))
+        assert len(files) == 1
+        assert files[0].name == 'image1.jpg'
 
-        # assert not files
+    def test_list_files_should_not_list_files_given_there_are_no_files(self, folders_fixture):
+        self.user.getPhotosets.return_value = folders_fixture
+        folders_fixture[0].getPhotos.return_value = []
+        storage = FlickrStorage(self.config, Resiliently(self.config))
+        folders = list(storage.list_folders())
+        files = list(storage.list_files(folders[0]))
 
-    # # def test_upload_should_not_fetch_folder_given_its_cached(self, folders_fixture):
-        # # self.api.list_albums.return_value = folders_fixture
-        # # storage = GoogleStorage(self.config, self.api)
-        # # _ = list(storage.list_folders())
-        # # folder = folders_fixture[0]
-        # # storage.upload('/', folder['title'], 'micky.jpg', None)
+        assert not files
 
-        # # self.api.list_albums.assert_called_once()
-        # # self.api.create_album.assert_not_called()
-        # # self.api.upload.assert_called_once_with('/', 'micky.jpg', folder['id'])
+    def test_upload_should_not_create_folder_given_it_exists(self, folders_fixture):
+        self.user.getPhotosets.return_value = folders_fixture
+        folders_fixture[0].addPhoto = MagicMock()
+        storage = FlickrStorage(self.config, Resiliently(self.config))
+        folders = list(storage.list_folders())
+        storage.upload('/', folders[0].name, 'micky.jpg', None)
 
-    # # def test_upload_should_fetch_folder_given_its_not_cached(self, folders_fixture):
-        # # self.api.list_albums.return_value = folders_fixture
-        # # storage = GoogleStorage(self.config, self.api)
-        # # folder = folders_fixture[0]
-        # # storage.upload('/', folder['title'], 'micky.jpg', None)
+        self.mock_flickr_api.Photoset.create.assert_not_called()
+        folders_fixture[0].addPhoto.assert_called_once()
 
-        # self.api.list_albums.assert_called_once()
-        # self.api.create_album.assert_not_called()
-        # self.api.upload.assert_called_once_with('/', 'micky.jpg', folder['id'])
+    def test_upload_should_create_folder_given_it_doesnt_exist(self):
+        self.user.getPhotosets.return_value = []
+        self.mock_flickr_api.Photoset.create.return_value = MagicMock()
+        storage = FlickrStorage(self.config, Resiliently(self.config))
+        _ = list(storage.list_folders())
+        storage.upload('/', 'new', 'micky.jpg', None)
 
-    # def test_upload_should_create_folder_given_it_doesnt_exist(self, folders_fixture):
-        # self.api.list_albums.return_value = []
-        # self.api.create_album.return_value = folders_fixture[0]
-        # storage = GoogleStorage(self.config, self.api)
-        # folder = folders_fixture[0]
-        # storage.upload('/', folder['title'], 'micky.jpg', None)
-
-        # self.api.list_albums.assert_called_once()
-        # self.api.create_album.assert_called_once()
-        # self.api.upload.assert_called_once_with('/', 'micky.jpg', folder['id'])
+        self.mock_flickr_api.Photoset.create.assert_called_once()
