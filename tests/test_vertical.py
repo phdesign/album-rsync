36b5e56047037268dad461d71e0ab60fd60574f9
Paul Heasley <paul@phdesign.com.au>
2019-03-10 15:13:00 -0400

    More veriticals

--- tests/test_vertical.py
+++ tests/test_vertical.py
@@ -12,7 +12,6 @@ from album_rsync.__main__ import main
 @patch('sys.stdout', new_callable=StringIO)
 class TestVertical:
 
-    @pytest.mark.focus
     def test_should_list_files_as_tree(self, mock_stdout, mock_stderr):
         testargs = ["album-rsync", "fake", "--list-only"]
         with patch.object(sys, 'argv', testargs):
@@ -31,7 +30,6 @@ class TestVertical:
     └─── B File
 """
 
-    @pytest.mark.focus
     def test_should_list_files_as_csv(self, mock_stdout, mock_stderr):
         testargs = ["album-rsync", "fake", "--list-only", "--list-format=csv"]
         with patch.object(sys, 'argv', testargs):
@@ -44,4 +42,45 @@ B Folder,A File,
 B Folder,B File,
 C Folder,A File,
 C Folder,B File,
+"""
+
+    def test_should_sync(self, mock_stdout, mock_stderr):
+        testargs = ["album-rsync", "fake", "fake"]
+        with patch.object(sys, 'argv', testargs):
+            main()
+            assert mock_stderr.getvalue() == """\
+building folder list...
+
+transferred 3 file(s), skipped 2 files(s) that already exist in 0.0 sec
+"""
+
+            assert mock_stdout.getvalue() == """\
+A Folder/
+B Folder/
+B Folder/A File
+B Folder/B File
+C Folder/
+C Folder/A File
+"""
+
+    def test_should_mirror(self, mock_stdout, mock_stderr):
+        testargs = ["album-rsync", "fake", "fake", "--delete", "-n"]
+        with patch.object(sys, 'argv', testargs):
+            main()
+            assert mock_stderr.getvalue() == """\
+dry run enabled, no files will be copied
+building folder list...
+
+transferred 3 file(s), skipped 2 files(s) that already exist, deleted 1 additional files(s) in 0.0 sec
+"""
+
+            assert mock_stdout.getvalue() == """\
+A Folder/
+B Folder/
+B Folder/A File
+B Folder/B File
+C Folder/
+C Folder/A File
+deleting D Folder/A File
+deleting D Folder/
 """
